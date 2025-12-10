eea1c8b5d253ee7ee415009884a44fa2f8779f72
Cường Nguyễn <cuongnguyen592005@gmail.com>
2025-12-10 14:03:32 +0700

    ĐDot

--- Back_end/gateway_service/src/main/java/com/gateway_service/GatewayApplication.java
+++ Back_end/gateway_service/src/main/java/com/gateway_service/GatewayApplication.java
@@ -1,12 +1,13 @@
 package com.gateway_service;
 
+import com.gateway_service.config.RateLimitProperties;
 import com.gateway_service.config.ServiceEndpointsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(ServiceEndpointsProperties.class)
+@EnableConfigurationProperties({ServiceEndpointsProperties.class, RateLimitProperties.class})
 public class GatewayApplication {
 
 	public static void main(String[] args) {
